c218677996649971a7db71976468d6be645011e4
Alex Beattie <alex@ourself.health>
2025-07-14 15:34:05 -0700

    Initial Commit

---
+++ Scoutable/ScoutableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutableApp.swift
+//  Scoutable
+//
+//  Created by Alex Beattie on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoutableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
